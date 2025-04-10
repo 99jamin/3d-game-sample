beceee6d359888d971493e726df2587ee8d3a5ae
Junil Ko <junil@mocapot.com>
2025-04-10 09:50:18 +0900

    상태 패턴으로 enemy 구현 중

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,5 +10,144 @@ public enum EnemyState { None, Idle, Patrol, Trace, Attack, Hit, Dead }
 [RequireComponent(typeof(Animator))]
 public class EnemyController : MonoBehaviour
 {
+    [Header("Basic Info")]
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private int attackPower = 10;
+    [SerializeField] private int defensePower = 5;
+    
+    [Header("AI")]
+    [SerializeField] private float detectCircleRadius = 10f;
+    public float DetectCircleRadius => detectCircleRadius;
+    [SerializeField] private LayerMask targetLayerMask;
+    [SerializeField] private float maxDetectSightAngle = 30f;
+    [SerializeField] private float maxPatrolWaitTime = 3f;
+    public float MaxPatrolWaitTime => maxPatrolWaitTime;
+    
+    // -----
+    // 상태 변수
+    private EnemyStateIdle _enemyStateIdle;
+    private EnemyStatePatrol _enemyStatePatrol;
+    private EnemyStateTrace _enemyStateTrace;
+    private EnemyStateAttack _enemyStateAttack;
+    private EnemyStateHit _enemyStateHit;
+    private EnemyStateDead _enemyStateDead;
+    
+    public EnemyState CurrentState { get; private set; }
+    private Dictionary<EnemyState, IEnemyState> _enemyStates;
+    
+    // -----
+    // 컴포넌트
+    public Animator EnemyAnimator { get; private set; }
+    public NavMeshAgent Agent { get; private set; }
+    
+    // -----
+    // 일반 멤버
+    private int _currentHealth;
 
+    private void Awake()
+    {
+        EnemyAnimator = GetComponent<Animator>();
+        Agent = GetComponent<NavMeshAgent>();
+        Agent.updateRotation = true;
+        Agent.updatePosition = false;
+    }
+
+    private void Start()
+    {
+        // 상태 객체 생성
+        _enemyStateIdle = new EnemyStateIdle();
+        _enemyStatePatrol = new EnemyStatePatrol();
+        _enemyStateTrace = new EnemyStateTrace();
+        _enemyStateAttack = new EnemyStateAttack();
+        _enemyStateHit = new EnemyStateHit();
+        _enemyStateDead = new EnemyStateDead();
+
+        _enemyStates = new Dictionary<EnemyState, IEnemyState>
+        {
+            { EnemyState.Idle, _enemyStateIdle },
+            { EnemyState.Patrol, _enemyStatePatrol },
+            { EnemyState.Trace, _enemyStateTrace },
+            { EnemyState.Attack, _enemyStateAttack },
+            { EnemyState.Hit, _enemyStateHit },
+            { EnemyState.Dead, _enemyStateDead }
+        };
+        
+        // HP 초기화
+        _currentHealth = maxHealth;
+        
+        // 상태 초기화
+        SetState(EnemyState.Idle);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void SetState(EnemyState newState)
+    {
+        if (CurrentState != EnemyState.None)
+        {
+            _enemyStates[CurrentState].Exit();
+        }
+        CurrentState = newState;
+        _enemyStates[CurrentState].Enter(this);
+    }
+
+    #region 이동 관련
+
+    private void OnAnimatorMove()
+    {
+        Vector3 position = EnemyAnimator.rootPosition;
+        
+        position.y = Agent.nextPosition.y;
+        
+        Agent.nextPosition = position;
+        transform.position = position;
+    }
+
+    public void PlayStep()
+    {
+        
+    }
+
+    #endregion
+
+    #region Player 감지 관련
+
+    // 일정 반경에 플레이어가 진입하면 플레이어 소리를 감지했다고 판단
+    public Transform DetectPlayerInCircle()
+    {
+        var hitColliders = Physics.OverlapSphere(transform.position, 
+            detectCircleRadius, targetLayerMask);
+        if (hitColliders.Length > 0)
+        {
+         return hitColliders[0].transform;
+        }
+        else
+        {
+         return null;
+        }
+    }
+
+    #endregion
+    
+    #region 디버깅
+
+     private void OnDrawGizmos()
+     {
+         // Circle 감지 범위
+         Gizmos.color = Color.yellow;
+         Gizmos.DrawWireSphere(transform.position, detectCircleRadius);
+         
+         // 시야각
+         Gizmos.color = Color.red;
+         Vector3 rightDirection = Quaternion.Euler(0, maxDetectSightAngle, 0) * transform.forward;
+         Vector3 leftDirection = Quaternion.Euler(0, -maxDetectSightAngle, 0) * transform.forward;
+         Gizmos.DrawRay(transform.position, rightDirection * detectCircleRadius);
+         Gizmos.DrawRay(transform.position, leftDirection * detectCircleRadius);
+         Gizmos.DrawRay(transform.position, transform.forward * detectCircleRadius);
+     }
+
+     #endregion
 }
--- Assets/Scripts/Enemy/EnemyState/EnemyStatePatrol.cs
+++ Assets/Scripts/Enemy/EnemyState/EnemyStatePatrol.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyStatePatrol : IEnemyState
 {
+    private EnemyController _enemyController;
+    
     public void Enter(EnemyController enemyController)
     {
+        _enemyController = enemyController;
         
+        _enemyController.EnemyAnimator.SetBool("Patrol", true);
     }
 
     public void Update()
@@ -14,6 +19,24 @@ public class EnemyStatePatrol : IEnemyState
 
     public void Exit()
     {
-        
+        _enemyController.EnemyAnimator.SetBool("Patrol", false);
+        _enemyController = null;
+    }
+    
+    private Vector3 FindRandomPatrolPoint()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * _enemyController.DetectCircleRadius;
+        randomDirection += _enemyController.transform.position;
+
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomDirection, out hit, 
+                _enemyController.DetectCircleRadius, NavMesh.AllAreas))
+        {
+            return hit.position;
+        }
+        else
+        {
+            return _enemyController.transform.position;            
+        }
     }
 }
